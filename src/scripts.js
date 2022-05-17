8d6095af48a783f51df8cd20a7dc288031e78cf0
Eric Li <ericlitong@gmail.com>
2022-05-17 15:13:19 +0800

    update

--- src/scripts.js
+++ src/scripts.js
@@ -2,3 +2,15 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("myDiv").style.display = "block";
+}
